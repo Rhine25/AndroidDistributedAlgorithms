7c58347cd3e91d849a2d1ce10552d81b6883a24b
Célia Dehors <celia.dehors@gmail.com>
2017-10-09 21:20:05 +0200

    Ask for permission and turn bluetooth on

--- AndroidComm/app/src/main/java/com/jujucecedudu/androidcomm/MainActivity.java
+++ AndroidComm/app/src/main/java/com/jujucecedudu/androidcomm/MainActivity.java
@@ -1,13 +1,27 @@
 package com.jujucecedudu.androidcomm;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_ENABLE_BLUETOOTH = 12;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BluetoothAdapter myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(myBluetoothAdapter == null){
+            //device does not support bluetooth
+        }
+
+        if(!myBluetoothAdapter.isEnabled()){
+            Intent enableBluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BLUETOOTH);
+        }
     }
 }
