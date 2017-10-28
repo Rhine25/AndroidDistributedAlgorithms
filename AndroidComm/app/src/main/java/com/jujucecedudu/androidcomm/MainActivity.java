454d7a80f8f0c833a3ffd0839ea0fad81c0b8577
Célia Dehors <celia.dehors@gmail.com>
2017-10-28 17:39:39 +0200

    A little cleaning and beautifying

--- AndroidComm/app/src/main/java/com/jujucecedudu/androidcomm/MainActivity.java
+++ AndroidComm/app/src/main/java/com/jujucecedudu/androidcomm/MainActivity.java
@@ -13,11 +13,21 @@ import android.util.Log;
 import android.widget.ProgressBar;
 
 import java.util.Set;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "BLUETOOTH_TEST_MAIN";
+
+    public static final UUID MY_UUID = UUID.fromString("7255562d-a5db-43d8-a38d-874453bc589b");
+
+    private static final int APP_DISCOVERY = 0;
+    private static final int APP_DISCOVERABLE = 1;
+    private static final int APP_CONNECT = 2;
+
+    private static final int MODE = APP_CONNECT;
 
     private static final int REQUEST_ENABLE_BLUETOOTH = 12;
-    //private static final int REQUEST_DISCOVERABLE = 22;
+    private static final int REQUEST_DISCOVERABLE = 22;
 
     private ProgressDialog progressDialog;
 
@@ -37,25 +47,27 @@ public class MainActivity extends AppCompatActivity {
             startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BLUETOOTH);
         }
 
-        //make the device discoverable
-        /*Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-        startActivityForResult(discoverableIntent, REQUEST_DISCOVERABLE);
-        Log.i("BLUETOOTH_TEST", "Discoverable");*/
-
         //get paired devices
         Set<BluetoothDevice> pairedDevices = myBluetoothAdapter.getBondedDevices();
         if(pairedDevices.size() > 0){
             for(BluetoothDevice device : pairedDevices){
                 String deviceName = device.getName();
                 String deviceMAC = device.getAddress();
-                Log.i("BLUETOOTH_TEST", "Paired device : " + deviceName + " " + deviceMAC);
+                Log.i(TAG, "Paired device : " + deviceName + " " + deviceMAC);
             }
         }
         else{
-            Log.i("BLUETOOTH_TEST", "No paired device");
+            Log.i(TAG, "No paired device");
         }
 
-        //Get info about discovered devices
+        //make the device discoverable
+        if(MODE == APP_DISCOVERABLE) {
+            Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            startActivityForResult(discoverableIntent, REQUEST_DISCOVERABLE);
+            Log.i(TAG, "Discoverable");
+        }
+
+        //Register to get info about system discovering devices
         IntentFilter filter = new IntentFilter();
         filter.addAction(BluetoothDevice.ACTION_FOUND);
         filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
@@ -67,48 +79,22 @@ public class MainActivity extends AppCompatActivity {
         progressDialog.setCancelable(false);
 
         //discover devices
-        if(myBluetoothAdapter.startDiscovery()) {
-            Log.i("BLUETOOTH_TEST", "Launched discovery");
-            //it is asynchronous so the discovery is not instantaneous
-        }
-        else{
-            Log.i("BLUETOOTH_TEST", "Discovery could not launch");
-        }
+        if(MODE == APP_DISCOVERY) {
+            if (myBluetoothAdapter.startDiscovery()) {
+                Log.i(TAG, "Launched discovery");
+                //it is asynchronous so the discovery is not instantaneous
+            } else {
+                Log.i(TAG, "Discovery could not launch");
+            }
 
-        /*switch (myBluetoothAdapter.getState()){
-            case BluetoothAdapter.STATE_OFF:
-                Log.i("BLUETOOTH_TEST", "Current state : Off");
-                break;
-            case BluetoothAdapter.STATE_ON:
-                Log.i("BLUETOOTH_TEST", "Current state : On");
-                break;
-            case BluetoothAdapter.STATE_TURNING_OFF:
-                Log.i("BLUETOOTH_TEST", "Current state : Turning Off");
-                break;
-            case BluetoothAdapter.STATE_TURNING_ON:
-                Log.i("BLUETOOTH_TEST", "Current state : Turning On");
-                break;
-        }
-        switch (myBluetoothAdapter.getScanMode()){
-            case BluetoothAdapter.SCAN_MODE_NONE:
-                Log.i("BLUETOOTH_TEST", "Current scan mode : None");
-                break;
-            case BluetoothAdapter.SCAN_MODE_CONNECTABLE:
-                Log.i("BLUETOOTH_TEST", "Current scan mode : Connectable");
-                break;
-            case BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE:
-                Log.i("BLUETOOTH_TEST", "Current scan mode : Connectable Discoverable");
-                break;
+            //myBluetoothAdapter.cancelDiscovery();
+            //here should pair to a device after stopping discovery process
         }
 
-        Log.i("BLUETOOTH_TEST", "Currently enabled ? " + myBluetoothAdapter.isEnabled());
-        Log.i("BLUETOOTH_TEST", "Currently discovering ? " + myBluetoothAdapter.isDiscovering());*/
+        if(MODE == APP_CONNECT && pairedDevices.size() > 0){
+            Log.i(TAG, "I'm gonna connect");
 
-        //here should pair to a device
-
-
-        //then stop discovery as it is resource consuming
-        //myBluetoothAdapter.cancelDiscovery();
+        }
     }
 
     private final BroadcastReceiver myReceiver = new BroadcastReceiver() {
@@ -119,18 +105,18 @@ public class MainActivity extends AppCompatActivity {
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
                 String deviceName = device.getName();
                 String deviceMAC = device.getAddress();
-                Log.i("BLUETOOTH_TEST", "Discovered device : " + deviceName + " " + deviceMAC);
+                Log.i(TAG, "Discovered device : " + deviceName + " " + deviceMAC);
             }
             else if(BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)){
-                Log.i("BLUETOOTH_TEST", "onReceive, discovery started");
+                Log.i(TAG, "onReceive, discovery started");
                 progressDialog.show();
             }
             else if(BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)){
-                Log.i("BLUETOOTH_TEST", "onReceive, discovery finished");
+                Log.i(TAG, "onReceive, discovery finished");
                 progressDialog.dismiss();
             }
             else{
-                Log.i("BLUETOOTH_TEST", "onReceive, action is not handled : " + action);
+                Log.i(TAG, "onReceive, action is not handled : " + action);
             }
         }
     };
@@ -141,6 +127,6 @@ public class MainActivity extends AppCompatActivity {
 
         unregisterReceiver(myReceiver);
 
-        Log.i("BLUETOOTH_TEST", "Unregistered receiver");
+        Log.i(TAG, "Unregistered receiver");
     }
 }
