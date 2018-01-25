403b7a28d00ac00aed25c64b76a9bcbed27e892d
Justine Romero <romerojustine90@gmail.com>
2018-01-25 16:51:50 +0100

    Update API.java change on sendMessage and onMessage function

--- AndroidComm/app/src/main/java/com/jujucecedudu/androidcomm/API.java
+++ AndroidComm/app/src/main/java/com/jujucecedudu/androidcomm/API.java
@@ -1,7 +1,9 @@
 package com.jujucecedudu.androidcomm;
 
 import android.util.Log;
-
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import static com.jujucecedudu.androidcomm.API.MessageTypes.*;
 
 /**
@@ -9,71 +11,59 @@ import static com.jujucecedudu.androidcomm.API.MessageTypes.*;
  */
 
 public class API {
-
-    /*AlgoLamportChat algo;
-
-    public API(AlgoLamportChat a){
-        algo = a;
-    }*/
-
     private static final String TAG = "BLUETOOTH_TEST_API";
 
     private static MyBluetoothService myBluetoothService;
+    private AlgoLamportChat algo;
 
     public interface MessageTypes{
-        byte TYPE_EXAMPLE_1 = 0x10;
-        byte TYPE_EXAMPLE_2 = 0x11;
-        byte TYPE_EXAMPLE_3 = 0x12;
-        byte REQ = 0x13;
-        byte ACK = 0x14;
-        byte REL = 0x15;
+        byte SEND_MESSAGE = 0x10;
+        byte type2 = 0x11;
+        byte type3 = 0x12;
     }
 
-    public static void initApi(MyBluetoothService bluetoothService){
-        myBluetoothService = bluetoothService;
+    public API(AlgoLamportChat a) {
+        algo = a;
+    }
+
+    public static void initApi(MyBluetoothService myBluetoothService2){
+        myBluetoothService = myBluetoothService2;
     }
 
     public static void onMessage(byte[] message){
         byte messageType = message[0];
+        MessagePacket mp = Utils.getMessagePacketFromByteMessage(message);
         switch(messageType){
-            case TYPE_EXAMPLE_1:
-                Log.i(TAG, "Received message example 1");
+            case SEND_MESSAGE:
+                //MET TA PUTAIN DE FONCTION ICI
+                Log.i(TAG, "Send message");
                 break;
-            case TYPE_EXAMPLE_2:
-                Log.i(TAG, "Received message example 2");
+            case type2:
+                Log.i(TAG, " message");
                 break;
-            case TYPE_EXAMPLE_3:
-                Log.i(TAG, "Received message example 3");
-                break;
-            case REQ:
-                //receiveREQ(emmeteur, horloge);
-                Log.i(TAG, "Received message example 3");
-                break;
-            case ACK:
-                //receiveACK(emmeteur, horloge);
-                Log.i(TAG, "Received message example 3");
-                break;
-            case REL:
-                //receiveREL(emmeteur, horloge);
-                Log.i(TAG, "Received message example 3");
+            case type3:
+                Log.i(TAG, " message");
                 break;
             default:
-                //TODO message d'erreur
-                Log.e(TAG, "Unknown message type : " + messageType);
+                Log.i(TAG, "onMessage, message type is not handled : " + messageType);
         }
     }
 
-    public void sendMessage(String dest, String tMess, Object data){
-        byte[] data_b = Utils.convertObjectToByteArray(data);
+    public void sendMessage(String dest, byte tMess, Object data){
+        byte[] d = Utils.convertObjectToByteArray(data);
+        byte[] data_b = new byte[d.length+1];
+        data_b[0] = tMess;
+        for(int i=1;i<data_b.length;i++)
+            data_b[i-1]=d[i-1];
         MessagePacket mp = new MessagePacket(myBluetoothService.getMyMAC(),dest,data_b);
         myBluetoothService.sendMessage(mp);
     }
-    
+
     public String[] getDevicesMACs(){
         return myBluetoothService.getAllDevicesMACs();
     }
-    
-     public int getNbEntries(){
+
+    public int getNbEntries(){
         return myBluetoothService.getNbEntries();
     }
 }
